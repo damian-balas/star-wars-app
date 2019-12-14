7245f147dd994d96ee60f419f0e2dbb001ac3b7e
damian-balas <damian.webdev@gmail.com>
2019-12-14 12:16:51 +0100

    Updates

---
+++ precache-manifest.ca2a9442a5fcc3f35f2fb7959b034831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef983c20ad94eb5d1de5506baa10e6a",
+    "url": "/star-wars-app/index.html"
+  },
+  {
+    "revision": "d6d89b18e382a2525c48",
+    "url": "/star-wars-app/static/js/2.f0e6532b.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/star-wars-app/static/js/2.f0e6532b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db6c652318c229697306",
+    "url": "/star-wars-app/static/js/main.d16b0309.chunk.js"
+  },
+  {
+    "revision": "01a2107dd4003d5c4b11",
+    "url": "/star-wars-app/static/js/runtime-main.9cecceb6.js"
+  },
+  {
+    "revision": "9af739f3d88e62dc61eda9fb9d654252",
+    "url": "/star-wars-app/static/media/bg.9af739f3.jpg"
+  },
+  {
+    "revision": "68f30c6e247bafef04f57f4d5421d607",
+    "url": "/star-wars-app/static/media/cardBg.68f30c6e.jpg"
+  }
+]);
